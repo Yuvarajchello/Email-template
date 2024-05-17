a60815f76bd9c021ebcc42bb3e2b652e4640b864
Yuvarajchello <148035131+Yuvarajchello@users.noreply.github.com>
2024-05-17 20:52:27 +0530

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,30 @@
+/* fonts */
+export const FontFamily = {
+  interRegular: "Inter-Regular",
+  interMedium: "Inter-Medium",
+  interSemiBold: "Inter-SemiBold",
+  interLight: "Inter-Light",
+  inriaSansBold: "InriaSans-Bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_3xs: 10,
+  size_sm: 14,
+  size_xs: 12,
+  size_15xl_3: 34,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorGray_100: "rgba(0, 0, 0, 0.5)",
+  colorGray_200: "rgba(0, 0, 0, 0.49)",
+  colorBlue: "#0018f5",
+  colorBlack: "#000",
+  fFC529: "#ffc529",
+  fE724C: "#fe724c",
+};
+/* border radiuses */
+export const Border = {
+  br_3xs: 10,
+  br_5xs_1: 7,
+};
